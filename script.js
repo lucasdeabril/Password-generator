6d986aaf3d86ac44a789e760e63525625f512f10
lucasdeabril <118875218+lucasdeabril@users.noreply.github.com>
2023-03-02 21:47:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const slyder = document.getElementById('range')
+const letras = document.getElementById('valor')
 
-slyder.oninput = (() => {
-    const letras = document.getElementById('valor')
-    let valor = slyder.value
-    letras.textContent = valor
-    console.log(valor)
-})
+
+function letras (){
+
+    let valor = slyder.value;
+    letras.textContent = valor;
+    console.log('valor');
+};
     
+function gerarsenha (){
+    window.alert('olá')
+}
+function oi(){
+    alert('olá mundo')
+}
